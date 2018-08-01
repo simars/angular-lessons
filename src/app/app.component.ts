9dd62fd25fcfa829e91c158fd5cf387d5cd710f7
Simar Singh <simarpaulsingh@gmail.com>
2018-08-01 09:18:12 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-lesson-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-lessons';
+}
